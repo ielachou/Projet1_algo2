4def60b7ebb7b7548d84b3d1552bac178dc62906
ielachou <iliass.elachouchi@gmail.com>
2018-10-21 14:39:38 +0200

    Affichage arbre

--- Tree.py
+++ Tree.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Tree():
     def __init__(self, root,val, children = []):
         self.root = root
@@ -6,6 +9,7 @@ class Tree():
         self.children = children
         #Sum of all of nodes's values
         self.sum = 0
+        self.graph = nx.Graph()
 
     def getVal(self):
         return self.val
@@ -19,8 +23,23 @@ class Tree():
     def deepSearch(self):
         pass
 
-    def printTree(self):
-        pass
+
+    def printTree(self,G, a =[]):
+        print(self.root)
+        a.append(self.root)
+        for c in self.children:
+            G.add_edge(c.root,self.root)
+            c.printTree(G, a)
+        return a
+
+    def printGraph(self):
+        G = nx.Graph()
+        NodeList = self.printTree(G)
+        print(NodeList)
+        G.add_nodes_from(NodeList)
+        nx.draw(G, with_labels = True)
+        plt.show()
 
 
 a = Tree("r", 2, [Tree("a", -5,[Tree("c", 4),Tree("d",-1),Tree("e", -1)]),Tree("b",-1, [Tree("f", -1), Tree("g", -2), Tree("h",2)])])
+a.printGraph()
