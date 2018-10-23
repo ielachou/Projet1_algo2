f4136e157d39f332e417cdc173659ccb8bf00ce3
alaamiri <32962513+alaamiri@users.noreply.github.com>
2018-10-23 15:20:41 +0200

    htutrtijgri

--- tree_bis.py
+++ tree_bis.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from random import randint
 
 class Tree():
 
-    def __init__(self, root, val, children = []):
+    def __init__(self, root = None, val = None, children = []):
         self.root = root
         self.val = val
         #each child is in the list
@@ -31,9 +32,9 @@ class Tree():
     def MakeGraph(self,G =nx.Graph()): #Anciennement PrintTree
         #print(s)elf.root)
         #On ajoute un tuple dans la liste (noeud, valeur)
-        G.add_node(self.root, val = self.val)
-        for c in self.children:
-            G.add_edge(c.root,self.root)
+        G.add_node(self.getRoot(), val = self.getVal())
+        for c in self.getChildren():
+            G.add_edge(c.getRoot(),self.getRoot())
             c.MakeGraph(G)
         return G
 
@@ -44,7 +45,7 @@ class Tree():
         plt.figure()
         pos_nodes = nx.spring_layout(G)
         pos_nodes = self.setCoord(pos_nodes)
-        nx.draw(G, pos_nodes, with_labels=True)
+        nx.draw(G, pos_nodes, node_color = "#B4EB00", font_weight = "bold",font_family = "futura", with_labels=True)
         #On va juste prendre les cordonnées pour pouvoir placer le label
         pos_attrs = {}
         for node, coords in pos_nodes.items():
@@ -63,11 +64,11 @@ class Tree():
 
 
     def setCoord(self, coord, width = 1., dy = 0.2, x = 0.5, y = 0):
-        coord[self.root] = np.array([x, y])
-        if len(self.children) != 0:
-            dx = width / len(self.children)
+        coord[self.getRoot()] = np.array([x, y])
+        if len(self.getChildren()) != 0:
+            dx = width / len(self.getChildren())
             newx = x - width / 2 - dx / 2
-        for i in range(len(self.children)):
+        for i in range(len(self.getChildren())):
             newx+=dx
             self.children[i].setCoord(coord, width = dx, dy = dy, x = newx, y =y - dy)
         return coord
@@ -87,12 +88,36 @@ class Tree():
             else:
                 self.children[i].max_subtree(G, somme)
             i+=1
-        return G
+
+def randomTree(tree = Tree("R", randint(-5, 5)), n = 12, birthRate = 3):
+    if n == 0:
+        return tree
+    else:
+        birth = 3
+        while n - birth < 0:
+            birth -=1
+
+        else:
+            children = []
+            for i in range(birth):
+                children.append(Tree(chr(97 + i), randint(-5,5)))
+        n -= birth
+        tree.children = children
+        for child in children:
+            tree = randomTree(child,n,birthRate)
+
+        return tree
 
 
 
-a = Tree("r", 2, [Tree("a", -5,[Tree("c", 4),Tree("d",-1,[Tree("i",4),Tree("j", -5, [Tree("l", -1), Tree("m", 3, [Tree("n", 1)])])]),Tree("e", -1)]),Tree("b",-1, [Tree("f", -1), Tree("g", -2, [Tree("k", 1)]), Tree("h",2)])])
+
+a = Tree("r", 2, [Tree("a", -5,[Tree("c", 4),Tree("d",-1,[Tree("i",4),Tree("j", -5, [Tree("l", -1), Tree("m", 3, [Tree("n", -1)])])]),Tree("e", -1)]),Tree("b",-1, [Tree("f", -1), Tree("g", -2, [Tree("k", 1)]), Tree("h",2)])])
 
 a.printGraph()
 G2 = a.max_subtree(a.graph)
 a.printGraph()
+
+"""
+t = randomTree()
+t.printGraph()
+"""
