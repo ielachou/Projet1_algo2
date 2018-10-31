a9641e3c5cde57b0a09ae1263e3e1972a7ff4935
Achraf <laamiri-achraf@hotmail.com>
2018-10-31 10:04:07 +0100

    Amélioration partie 2

--- hypergraph.py
+++ hypergraph.py
@@ -7,15 +7,23 @@ import numpy as np
 Matrice d'incidence pour afficher l'hypergraphe
 """
 class Hypergraph():
-    def __init__(self, M = [[]], type = "I"):
-        self.mat = M
+    def __init__(self, M = [[]], type = ""):
 
+        self.mat = M
         self.nodes = len(M)
         self.edges = len(M[0])
+
+        self.incidence = self.incidenceHG()
+        self.primal = self.primalHG()
+        #self.test_hypertree()
+        """
         if type == "I":
-            self.incidence = self.incidenceHG()
+            self.drawHG(self.incidence[1])
         elif type == "P":
-            self.primal = self.primalHG()
+            self.drawHG(self.primal[1])
+        else:
+            self.drawHG()
+        """
 
     def getNbrNodes(self):
         return self.nodes
@@ -34,19 +42,17 @@ class Hypergraph():
         j = 0
         for i in range(len(M)):
             HG.add_node("v" + str(i+1))
-            pos_nodes["v" + str(i+1)] = np.array([0, 10 - i])
+            pos_nodes["v" + str(i+1)] = np.array([5, 10 - i])
 
             for j in range(len(M[0])):
                 if M[i][j]:
                     curr_edge = "E" + str(j+1)
                     if curr_edge not in HG.edges():
                         HG.add_node("E" + str(j+1))
-                    pos_nodes["E" + str(j+1)] = np.array([5, 10 - j])
+                    pos_nodes["E" + str(j+1)] = np.array([0 , 10 - j])
                     HG.add_edge("v" + str(i+1),"E" + str(j+1))
-
+        fig = plt.figure("Graphe d'incidence")
         nx.draw(HG, pos_nodes, with_labels = True, node_color = "#61FDD9", node_size = 600)
-        plt.show()
-
         return HG
 
     def primalHG(self):
@@ -56,7 +62,7 @@ class Hypergraph():
         for i in range(len(M[0])):
             same_edge = []
             for j in range(len(M)):
-                #####OPTIMISER##################################""
+                #####OPTIMISER##################################
                 if not 1 in M[j]:
                     HG.add_node("v" + str(j+1))
                 if M[j][i] == 1:
@@ -68,12 +74,55 @@ class Hypergraph():
             elif len(same_edge) == 1:
                 HG.add_node(same_edge[0])
 
+        fig = plt.figure("Graphe primal")
         nx.draw(HG, with_labels = True, node_color = "#FFFFFF", node_size = 600)
-        print("Cliques : ", list(nx.find_cliques(HG)))
-        print("Chordal ? : ", nx.is_chordal(HG))
-
-        plt.show()
         return HG
+
+    def checkClique(self):
+        primal = self.primal
+        incidence = self.incidence
+        dic_edge = {}
+        for i in range(self.edges):
+            E_i = "E" + str(i+1)
+            curr_edges = incidence.edges(E_i)
+            print("Hyper-arềte : ",curr_edges)
+            if len(curr_edges) >= 2:
+                for edge in curr_edges:
+                    if E_i not in dic_edge:
+                        dic_edge[E_i] = [edge[1]]
+                    else:
+                        dic_edge[E_i].append(edge[1])
+
+        print(dic_edge)
+        """
+        cliques = list(nx.find_cliques(primal))
+        res = True
+        while len(cliques) != 0:
+            clique = cliques.pop()
+            if len(clique) >= 2:
+                for node in clique:
+                    for edge in dic_edge:
+                        if node in edge
+        """            
+    def test_hypertree(self):
+        primal = self.primal
+
+
+
+        print("Cliques : ", list(nx.find_cliques(primal)))
+        print("Chordal ? : ", nx.is_chordal(primal))
+
+        if nx.is_chordal(primal):
+            self.checkClique()
+
+            plt.show()
+        else:
+            plt.show()
+            return False
+
+    def drawHG(self):
+        plt.show()
+
 
 
 def randomHypergraph(v = 0, E = 0):
@@ -81,3 +130,4 @@ def randomHypergraph(v = 0, E = 0):
         return np.random.randint(2, size=(randint(1,7),(randint(1,7))))
     else:
         return np.random.randint(2, size=(v,E))
+    plt.show()
--- test_hg.py
+++ test_hg.py
@@ -8,15 +8,15 @@ def test(n,type = "I"):
           [0,0,1,0],
           [0,0,1,0],
           [0,0,0,0]]
-
+    ex2 =[[1, 1, 1, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 0, 0, 0]]
     if n == 1 : #affcihage normal
-        test = hg.Hypergraph(ex,type)
+        test = hg.Hypergraph(ex2,type)
         test.test_hypertree()
-        #test.drawHG()
+        test.drawHG()
     elif n == 2: #affichage avec matrice aléatoire
         ex = hg.randomHypergraph()
         print(ex)
-        hg.Hypergraph(ex,type)
-
+        test = hg.Hypergraph(ex,type)
+        test.test_hypertree()
 if __name__ == '__main__':
     test(1)
