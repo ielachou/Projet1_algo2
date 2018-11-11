3f6b94f03f3a781a81f4553726b3975c32e51cae
ielachou <ielachou@ulb.ac.be>
2018-11-11 16:30:17 +0100

    opti main

--- algo2_2018.py
+++ algo2_2018.py
@@ -2,38 +2,32 @@ from tree import *
 from test_hg import *
 import matplotlib.pyplot as plt
 
+
 def main():
     rep = 0
-    while rep != 3:
-        print("Bienvenue sur le projet d'algo d'Achraf et Iliass !\n")
+    print("Bienvenue sur le projet d'algo d'Achraf et Iliass !\n")
+    while rep != "3":
         print("Sélectionnez la section du projet que vous voulez utiliser : \n"
               "1 : Sous-arbre de poids maximum\n"
               "2 : Les hypergraphes et hypertrees\n"
               "3 : Quitter\n")
         rep = ""
-        while rep not in ["1", "2","3"]:
+        while rep not in ["1", "2", "3"]:
             rep = input()
-            if rep not in ["1","2","3"]:
+            if rep not in ["1", "2", "3"]:
                 print("Entrez 1, 2 ou 3 !")
         if rep == "1":
             a = Tree("r", 2,
-                     [Tree("a", -5, [Tree("c", 4), Tree("d", -1, [Tree("i", 4), Tree("j", -5, [Tree("l", -1), Tree("m", 3, [
-                         Tree("n", -1)])])]), Tree("e", -1)]),
+                     [Tree("a", -5,
+                           [Tree("c", 4), Tree("d", -1, [Tree("i", 4), Tree("j", -5, [Tree("l", -1), Tree("m", 3, [
+                               Tree("n", -1)])])]), Tree("e", -1)]),
                       Tree("b", -1, [Tree("f", -1), Tree("g", -2, [Tree("k", 1, [Tree("z", 1)])]), Tree("h", 2)])])
             print("Exemple de l'énoncé, fermez la fenêtre pour accéder à la suite.")
-            a.printGraph()
-            a.max_subtree(a.graph)
-            a.printGraph(subbed=True)
-
-            plt.show()
-            a.graph.clear()
-            print("Exemple aléatoire, fermez la fenêtre pour accéder à la suite.")
+            exec(a)
             a = random_tree(Tree('r', randint(-5, 5)), 10)
-            a.printGraph()
-            a.max_subtree(a.graph)
-            a.printGraph(subbed=True)
-            plt.show()
-        elif rep =="2":
+            print("Exemple aléatoire, fermez la fenêtre pour accéder à la suite.")
+            exec(a)
+        elif rep == "2":
             print("Exemple de l'énoncé, fermez les fenêtres pour accéder à la suite.")
             test(1)
             print("Exemple aléatoire, fermez les fenêtres pour accéder à la suite.")
--- tree.py
+++ tree.py
@@ -13,31 +13,29 @@ class Tree():
             self.children = []
         else:
             self.children = children
-        # Sum of all of nodes's values
-        self.sum = 0
         self.graph = None
-
-    def add_child(self, tree):
-        self.children.append(tree)
 
     def getVal(self):
         return self.val
-
-    def get_subSum(self, res = 0):
-        if self.getChildren() == []:
-            res += self.getVal()
-        else:
-            res_t = 0
-            for c in self.getChildren():
-                res_t += c.get_subSum(res)
-            res+= self.getVal()+res_t
-        return res
 
     def getChildren(self):
         return self.children
 
     def getRoot(self):
         return self.root
+
+    def add_child(self, tree):
+        self.children.append(tree)
+
+    def get_subSum(self, res=0):
+        if self.getChildren() == []:
+            res += self.getVal()
+        else:
+            res_t = 0
+            for c in self.getChildren():
+                res_t += c.get_subSum(res)
+            res += self.getVal() + res_t
+        return res
 
     def MakeGraph(self, G=nx.Graph()):  # Anciennement PrintTree
         # print(s)elf.root)
@@ -48,16 +46,13 @@ class Tree():
             c.MakeGraph(G)
         return G
 
-    def __repr__(self):
-        return "Tree de père " + str(self.getRoot())+ " "
-
-    def printGraph(self, subbed = False):
+    def printGraph(self, subbed=False):
         G = self.MakeGraph()
-        #Create a matplot figure window
+        # Create a matplot figure window
         plt.figure(1)
         pos_nodes = nx.spring_layout(G)
         posy = -1 if subbed else 0
-        pos_nodes = self.setCoord(pos_nodes, y = posy)
+        pos_nodes = self.setCoord(pos_nodes, y=posy)
         dico_isolated = nx.floyd_warshall(G)['r']
         for val in dico_isolated:
             if dico_isolated[val] == float('inf'):
@@ -88,7 +83,6 @@ class Tree():
             self.children[i].setCoord(coord, width=dx, dy=dy, x=newx, y=y - dy)
         return coord
 
-
     def max_subtree(self, G):
         i = 0
         while i < len(self.getChildren()):
@@ -97,47 +91,34 @@ class Tree():
                 G.remove_node(self.getChildren()[i].getRoot())
                 del self.getChildren()[i]
 
-                i-=1
-            i+=1
+                i -= 1
+            i += 1
 
 
-def random_tree(tree, n, noms=['r'], c = 97):
-    if n >0:
-        nbChilds = randint(0,3)
+def random_tree(tree, n, noms=['r'], c=97):
+    if n > 0:
+        nbChilds = randint(0, 3)
         if nbChilds == 0 and tree.getRoot() == 'r':
-            nbChilds = randint(1,3)
-        n-=nbChilds
+            nbChilds = randint(1, 3)
+        n -= nbChilds
         for i in range(nbChilds):
-            char = chr(c+i)
+            char = chr(c + i)
             j = 1
             while char in noms:
-                char = chr(c+j+i)
-                j+=1
+                char = chr(c + j + i)
+                j += 1
             noms.append(char)
-            tree.add_child(random_tree(Tree(char,randint(-5,5)), n-i-1, noms, c+i+j))
+            tree.add_child(random_tree(Tree(char, randint(-5, 5)), n - i - 1, noms, c + i + j))
 
     return tree
 
 
+def exec(tree):
+    tree.printGraph()
+    print("Somme de l'arbre initial : " + str(tree.get_subSum()))
+    tree.max_subtree(tree.graph)
+    tree.printGraph(subbed=True)
+    print("Somme de l'arbre réduit : " + str(tree.get_subSum()))
 
-
-
-
-
-
-if __name__ == 'main':
-    a = Tree("r", 2,
-             [Tree("a", -5, [Tree("c", 4), Tree("d", -1, [Tree("i", 4), Tree("j", -5, [Tree("l", -1), Tree("m", 3, [
-                 Tree("n", -1)])])]), Tree("e", -1)]),
-              Tree("b", -1, [Tree("f", -1), Tree("g", -2, [Tree("k", 1, [Tree("z", 1)])]), Tree("h", 2)])])
-    a.printGraph()
-    a.max_subtree(a.graph)
-    a.printGraph(subbed=True)
     plt.show()
-    a.graph.clear()
-
-    a = random_tree(Tree('r',randint(-5,5)),10)
-    a.printGraph()
-    a.max_subtree(a.graph)
-    a.printGraph(subbed = True)
-    plt.show()
+    tree.graph.clear()
