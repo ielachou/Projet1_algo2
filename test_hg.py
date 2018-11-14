6970040be3bc81e86223151cb4e5ccca0cff9444
alaamiri <32962513+alaamiri@users.noreply.github.com>
2018-11-14 22:17:36 +0100

    Commentaire

--- test_hg.py
+++ test_hg.py
@@ -1,26 +1,6 @@
 import hypergraph as hg
 
 def test(n):
-    """
-    [[0 1 0 0]]
-Cliques :  [['v1']]
-Chordal ? :  True
-{}
-ok
-
-
-[[0]
- [0]
- [1]
- [1]
- [0]]
-Cliques :  [['Ev1']]
-Chordal ? :  True
-{'E1': ['Ev4', 'Ev3']}
-L'hypergraphe est un hypertree
-
-
-    """
     ex = [[1,0,0,0],
           [1,1,0,0],
           [1,1,1,0],
@@ -28,7 +8,7 @@ L'hypergraphe est un hypertree
           [0,0,1,0],
           [0,0,1,0],
           [0,0,0,0]]
-    ex2 =[[1, 1, 1, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 0, 0, 0]]
+
     if n == 1 : #affcihage normal
         test = hg.Hypergraph(ex)
         print(test.test_hypertree())
@@ -36,9 +16,9 @@ L'hypergraphe est un hypertree
 
     elif n == 2: #affichage avec matrice aléatoire
         ex = hg.randomHypergraph()
-        print(ex)
+        print("Matrice d'incidence: \n",ex,"\n")
         test = hg.Hypergraph(ex)
         print(test.test_hypertree())
 
 if __name__ == '__main__':
-    test(2)
+    test(1)
